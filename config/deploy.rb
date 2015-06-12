6de0a6de6e5134932822d4622f1958c5182f01cb
Chris Cassatt <chris.cassatt@gmail.com>
2015-06-12 14:05:02 -0400

    fixed github url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'SNAP'
-set :repo_url, 'https://github.com/STSILABS/SNAP.git'
+set :repo_url, 'https://github.com/cjcassatt/SNAP.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
